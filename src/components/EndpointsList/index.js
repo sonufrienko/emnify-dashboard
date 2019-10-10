7303394f81a69a6991eb4f06415e5d10b788a1cd
zakutynsky@yahoo.com <zakutynsky@yahoo.com>
2019-10-10 14:53:40 +0300

    Refactoring

--- src/components/EndpointsList/index.js
+++ src/components/EndpointsList/index.js
@@ -10,9 +10,8 @@ import TablePagination from '@material-ui/core/TablePagination';
 import Paper from '@material-ui/core/Paper';
 import get from 'lodash/get';
 import Collapse from "@material-ui/core/Collapse";
-import styled from 'styled-components';
-import { CloudDone, CloudOff, SimCard, MobileFriendly, ScreenLockPortrait } from '@material-ui/icons';
-
+import DataStructure from './dataStructure';
+import { SubFieldItem,  FieldLabel, TableCellStyles } from './styles';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -25,7 +24,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-function useWindowSize() {
+const useWindowSize = () => {
   const isClient = typeof window === 'object';
 
   function getSize() {
@@ -48,83 +47,9 @@ function useWindowSize() {
 
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
-  }, []); // Empty array ensures that effect is only run on mount and unmount
+  }, []);
 
   return windowSize;
-}
-
-
-const SubFieldItem = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  margin: 10px 0;
-`;
-
-const FieldLabel = styled.span`
-    font-weight: bold;
-    width: 110px;
-    text-align: right;
-    margin-right: 15px;
-    color: #4c4040;
-`;
-
-const DataStructure = {
-  main: [
-    {
-      title: 'Status',
-      pattern: 'status.id',
-      custom: (status) => (status === 0) ? <CloudDone/> : <CloudOff />
-    },
-    {
-      title: 'Name',
-      pattern: 'name'
-    },
-    {
-      title: 'ICCID',
-      pattern: 'sim.iccid',
-      custom: (value, size) => {
-        if (size.width <= 700) {
-          return (value) ? <SimCard/> : false;
-        } else return value;
-      } 
-    },
-    {
-      title: 'IMEI',
-      pattern: 'imei',
-      custom: (value, size) => {
-        if (size.width <= 700) {
-          return (value) ? <MobileFriendly/> : false;
-        } else return value;
-      } 
-    },
-    {
-      title: 'IMEI Lock',
-      pattern: 'imei_lock',
-      custom: (value) => (value) ? <ScreenLockPortrait/> : false
-    }
-  ],
-  subfields: [
-    {
-      pattern: "id",
-      title: "ID"
-    },
-    {
-      pattern: "sim.msisdn",
-      title: "MSISDN"
-    },
-    {
-      pattern: "ip_address",
-      title: "IP"
-    },
-    {
-      pattern: "service_profile.name",
-      title: "Service Profile"
-    },
-    {
-      pattern: "tariff_profile.name",
-      title: "Tariff Profile"
-    }
-  ]
 }
 
 export default function EndpointsList ({ endpoints, totalCount, page, perPage, handleChangePage, handleChangeRowsPerPage }) {
@@ -159,7 +84,7 @@ export default function EndpointsList ({ endpoints, totalCount, page, perPage, h
                       }) }
                     </TableRow>,
                     <TableRow  key={`${endpoint.id}_subfield`}>
-                    <TableCell style={{ padding: 0, paddingTop: 0, borderTop: 'none', background: 'rgba(128, 128, 128, 0.05)' }} colSpan={6}>
+                    <TableCell style={TableCellStyles} colSpan={6}>
                       <Collapse
                         in={collapsedRow === index}
                         timeout="auto"
